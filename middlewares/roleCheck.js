34fdacff18deb04610f874cb5f850c2a2b06b98c
jinu <100375452+jinu721@users.noreply.github.com>
2024-12-08 18:26:20 +0530

    Update roleCheck.js

--- middlewares/roleCheck.js
+++ middlewares/roleCheck.js
@@ -7,25 +7,25 @@ const userModal = require('../models/userModel');
 // - If the user is not logged in or does not have the 'admin' role, they will be redirected to appropriate pages.
 
 let roleCheck = async (req, res, next) => {
-    // const adminRoutes = [
-    //     '/admin',
-    //     '/admin/products',
-    //     '/admin/users',
-    //     '/admin/categories',
-    //     '/admin/dashboard',
-    //     '/admin/coupens',
-    //     '/admin/orders'
-    // ];
-    // if (adminRoutes.includes(req.url)) {
-    //     if (!req.session.loggedIn) {
-    //         return res.redirect('/register');  
-    //     }
-    //     const email = req.session.userEmail;
-    //     const user = await userModal.findOne({ email });
-    //     if (user.role !== "admin") {
-    //         return res.redirect('/'); 
-    //     }
-    // }
+    const adminRoutes = [
+        '/admin',
+        '/admin/products',
+        '/admin/users',
+        '/admin/categories',
+        '/admin/dashboard',
+        '/admin/coupens',
+        '/admin/orders'
+    ];
+    if (adminRoutes.includes(req.url)) {
+        if (!req.session.loggedIn) {
+            return res.redirect('/register');  
+        }
+        const email = req.session.userEmail;
+        const user = await userModal.findOne({ email });
+        if (user.role !== "admin") {
+            return res.redirect('/'); 
+        }
+    }
 
     return next();
 };
