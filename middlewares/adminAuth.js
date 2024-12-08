31f81de8e0efc44c85712d105dbb15b455d4d7a7
jinu <100375452+jinu721@users.noreply.github.com>
2024-12-08 18:25:48 +0530

    Update adminAuth.js

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -8,17 +8,17 @@
 // - Allows the request to proceed if the conditions are met.
 
 let adminCheck = (req, res, next) => {
-    // if (['/admin/dashboard', '/admin/users', '/admin/products','/admin/categories'].includes(req.url)) {
-    //     if (!req.session.AdminloggedIn) {
-    //         return res.redirect('/admin');
-    //     }
-    //     return next();
-    // } else if (req.url==='/admin') {
-    //     if (req.session.AdminloggedIn) {
-    //         return res.redirect('/admin/dashboard');
-    //     }
-    //     return next();
-    // }
+    if (['/admin/dashboard', '/admin/users', '/admin/products','/admin/categories'].includes(req.url)) {
+        if (!req.session.AdminloggedIn) {
+            return res.redirect('/admin');
+        }
+        return next();
+    } else if (req.url==='/admin') {
+        if (req.session.AdminloggedIn) {
+            return res.redirect('/admin/dashboard');
+        }
+        return next();
+    }
     return next();
 };
 
